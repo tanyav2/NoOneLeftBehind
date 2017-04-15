ee8af842e1dfb199ffae41f0d5bc6dfa4904c89d
Streamliner <jimmy.xrail@me.com>
2017-04-15 15:32:25 -0500

    [ECU] Usable SPI Protocol

---
+++ ecu_firmware/src/global_car.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "car.h"
+
+Car car;
--- ecu_firmware/src/main.cpp
+++ ecu_firmware/src/main.cpp
@@ -6,38 +6,13 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#include "car.h"
+#include "global_car.h"
+#include "async.h"
 
-Car car;
 
 int main() {
-    while(1) {
-        car.set_speed(2);
-        _delay_ms(2000);
-        car.set_speed(5);
-        _delay_ms(2000);
-        car.set_speed(8);
-        _delay_ms(2000);
-        car.restore(1, 0);
-        _delay_ms(2000);
-        car.set_speed(-8);
-        _delay_ms(2000);
-        car.restore(1, 0);
-        _delay_ms(2000);
-        car.set_heading(4);
-        _delay_ms(2000);
-        car.restore(1, 1);
-        _delay_ms(2000);
-        car.set_heading(-4);
-        _delay_ms(2000);
-        car.restore(1, 1);
-        _delay_ms(2000);
-    }
-    //
-    // PORTB |= _BV(PB6);
-    // PORTB &= ~(_BV(PB7));
-    // _delay_ms(2000);
-    // PORTB |= _BV(PB7);
-    // PORTB &= ~(_BV(PB6));
-    // _delay_ms(2000);
+    // Initialize Hardware
+    spi_init();
+    sei(); //Initialize interrupt and Program Start
+    while (1);
 }
