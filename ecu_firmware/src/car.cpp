ed342c3769816a7c8d081ee542f34d774329112c
Streamliner <jimmy.xrail@me.com>
2017-04-14 23:28:49 -0500

    [ECU] Complete Driving System

--- ecu_firmware/src/car.cpp
+++ ecu_firmware/src/car.cpp
@@ -1,22 +1,47 @@
 #include "car.h"
 
 #define DEBUG 1
+#define BAUD 9600
 
-Car::Car(uint8_t lf, uint8_t lb, uint8_t rf, uint8_t rb) {
-    Control_mux& p = this -> pins;
-    p.lf = lf;
-    p.lb = lb;
-    p.rf = rf;
-    p.rb = rb;
-    pinMode(lf, OUTPUT);
-    pinMode(lb, OUTPUT);
-    pinMode(rf, OUTPUT);
-    pinMode(rb, OUTPUT);
-    this -> refreshStates();
+#include <util/setbaud.h>
+
+/*
+  Controller Assignment
+  - Left: PB6, OC0B
+  - Right: PB7, OC0A
+  Logical Matrix
+        PB6 OC0B PB7 OC1A
+  FD    1   -    1   -
+  RW    0   +    0   +
+  LH    1   -    0   +
+  RH    0   +    1   -
+*/
+
+Car::Car() {
+    // GPIO Initialization
+    DDRB |= _BV(PB6) | _BV(PB7);
+    DDRD |= _BV(PD5) | _BV(PD6);
+    // Start Timer 0 as Fast PWM, sans-Prescaler
+    TCCR0A |= _BV(WGM00) | _BV(WGM01);
+    TCCR0B |= _BV(CS00);
+    // Non-inverting
+    TCCR0A |= _BV(COM0A1) | _BV(COM0B1);
+    this -> refresh_states();
+    #ifdef DEBUG
+    // Initialize debugger
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    #if USE_2X
+    UCSR0A |= _BV(U2X0);
+    #else
+    UCSR0A &= ~(_BV(U2X0));
+    #endif
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+    UCSR0B = _BV(TXEN0); // Tx only
+    #endif
 }
 
-Control_data Car::computeControlData() {
-    Control_data data;
+void Car::refresh_states() {
     int raw_l, raw_r;
     raw_l = (this -> speed) * 32;
     raw_r = raw_l;
@@ -28,75 +53,52 @@ Control_data Car::computeControlData() {
     raw_r = (raw_r < -255) ? -255 : raw_r;
     raw_r = (raw_r > 255) ? 255 : raw_r;
     if (raw_l > 0) {
-        data.lf = raw_l;
-        data.lb = 0;
+        PORTB |= _BV(PB6);
+        OCR0B = (uint8_t) (255 - raw_l);
     } else {
-        data.lf = 0;
-        data.lb = 0 - raw_l;
+        PORTB &= ~(_BV(PB6));
+        OCR0B = (uint8_t) (0 - raw_l);
     }
     if (raw_r > 0) {
-        data.rf = raw_r;
-        data.rb = 0;
+        PORTB |= _BV(PB7);
+        OCR0A = (uint8_t) (255 - raw_r);
     } else {
-        data.rf = 0;
-        data.rb = 0 - raw_r;
+        PORTB &= ~(_BV(PB7));
+        OCR0A = (uint8_t) (0 - raw_r);
     }
-    return data;
 }
 
-void Car::refreshStates() {
-    Control_data data = this -> computeControlData();
-    Control_mux& p = this -> pins;
-    analogWrite(p.lf, data.lf);
-    analogWrite(p.lb, data.lb);
-    analogWrite(p.rf, data.rf);
-    analogWrite(p.rb, data.rb);
-    #ifdef DEBUG
-        Serial.print("lf=");
-        Serial.print(data.lf);
-        Serial.print(" lb=");
-        Serial.print(data.lb);
-        Serial.print(" rf=");
-        Serial.print(data.rf);
-        Serial.print(" rb=");
-        Serial.print(data.rb);
-        Serial.print(" Speed=");
-        Serial.print(this -> speed);
-        Serial.print(" Heading=");
-        Serial.println(this -> heading);
-    #endif
-}
 
 void Car::steer(int accel, int yaw) {
-    int& h = this -> heading;
-    int& s = this -> speed;
+    int8_t& h = this -> heading;
+    int8_t& s = this -> speed;
     s += accel;
     h += yaw;
     s = (s > 8) ? 8 : (s < -8) ? -8 : s;
     h = (h > 8) ? 8 : (h < -8) ? -8 : h;
-    this -> refreshStates();
+    this -> refresh_states();
 }
 
 void Car::restore(uint8_t speed, uint8_t heading) {
     if (speed) this -> speed = 0;
     if (heading) this -> heading = 0;
-    this -> refreshStates();
+    this -> refresh_states();
 }
 
-int Car::getheading() {
+int Car::get_heading() {
     return this -> heading;
 }
 
-int Car::getspeed() {
+int Car::get_speed() {
     return this -> speed;
 }
 
-void Car::setspeed(int speed) {
+void Car::set_speed(int speed) {
     this -> speed = (speed > 8) ? 8 : (speed < -8) ? -8 : speed;
-    this -> refreshStates();
+    this -> refresh_states();
 }
 
-void Car::setheading(int heading) {
+void Car::set_heading(int heading) {
     this -> heading = (heading > 8) ? 8 : (heading < -8) ? -8: heading;
-    this -> refreshStates();
+    this -> refresh_states();
 }
